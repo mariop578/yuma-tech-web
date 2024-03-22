1a1eeb5b3191db2bc517c05c4b17bd09f02fa6fa
mariop578 <partidam578@gmail.com>
2024-03-22 16:31:15 -0700

    Created Comment Route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./userR");
 const postRoutes = require("./postR");
+const commentRoutes = require("./commentR");
 
 router.use("/user", userRoutes);
 router.use("/post", postRoutes);
-
+router.use("/comment", commentRoutes);
 module.exports = router;
