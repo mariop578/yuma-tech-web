2c515703aaa874e6a4d017c294d9e301d201f163
mariop578 <partidam578@gmail.com>
2024-03-22 19:25:28 -0700

    Created Handlebars for Login, Signup and Posts.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,4 +18,40 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        { model: User, attributes: ["username"] },
+        {
+          model: Comment,
+          include: [{ model: User, attributes: ["username"] }],
+        },
+      ],
+    });
+    const post = postData.get({ plain: true });
+    res.render("post", {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signup");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
